0c35b4cf1478a719669099477d21d7b6219d4ea2
abbratu <adrian@lenderauto.com>
2022-07-25 13:21:17 -0400

    ssr

--- projects/ng-ssr/src/app/app.module.ts
+++ projects/ng-ssr/src/app/app.module.ts
@@ -13,7 +13,7 @@ import { MatDividerModule } from '@angular/material/divider';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
 
     MatButtonModule,
